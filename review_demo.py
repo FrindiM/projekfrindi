3694c10e096ab051ec58fe6e3cb03aa50e606bf6
Frindi Mangimbulude <friendymangimbulude@gmail.com>
2022-10-16 18:13:10 +0700

    new file:   background.png 	new file:   bg.jpg 	new file:   png.png 	modified:   review_demo.py

--- review_demo.py
+++ review_demo.py
@@ -200,6 +200,7 @@ def TakeImages():
         if (name.isalpha() == False):
             res = "Enter Correct name"
             message.configure(text=res)
+            
 ########################################################################################
 #$$$$$$$$$$$$$
 def TrainImages():
@@ -365,14 +366,7 @@ def TrackImages():
             writer.writerow(col_names)
             writer.writerow(login)
         csvFile1.close()
-    # with open("Login\Login_" + date + ".csv", 'r') as csvFile1:
-    #     reader1 = csv.reader(csvFile1)
-    #     for lines in reader1:
-    #         i = i + 1
-    #         if (i > 1):
-    #             if (i % 2 != 0):
-    #                 iidd = str(lines[0]) + '   '
-    #                 tbl.insert('', 0, text=iidd, values=(str(lines[2]), str(lines[4]), str(lines[6])))
+        
     csvFile1.close()
     cam.release()
     cv2.destroyAllWindows()
@@ -386,7 +380,11 @@ window.title("Aplikasi login")
 window.overrideredirect(False)
 window.geometry("{0}x{1}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
 window.resizable(True,True)
-window.configure(background='#355454')
+bg = PhotoImage( file = "png.png")
+label1 = Label( window, image = bg)
+# label1.config(width=2000)
+label1.place(x = 0,y = 0)
+# window.configure(background='#355454')
 
 #nafigasi pindah frame
 def raise_frame(frame):
@@ -413,72 +411,27 @@ def halaman1():
     message3.place(x=10, y=10,relwidth=1)
 
     #frames-------------------------------------------------
-    frame1 = tkinter.Frame(window, bg="white")
-    frame1.place(relx=0.11, rely=0.15, relwidth=0.39, relheight=0.80)
+    
 
     frame2 = tkinter.Frame(window, bg="white")
     frame2.place(relx=0.51, rely=0.15, relwidth=0.39, relheight=0.80)
 
     #frame_headder
-    fr_head1 = tkinter.Label(frame1, text="Register", fg="white",bg="black" ,font=('times', 17, ' bold ') )
-    fr_head1.place(x=0,y=0,relwidth=1)
+    
 
     fr_head2 = tkinter.Label(frame2, text="Login", fg="white",bg="black" ,font=('times', 17, ' bold ') )
     fr_head2.place(x=0,y=0,relwidth=1)
 
-    #registretion frame
-    lbl = tkinter.Label(frame1, text="Enter ID",width=20  ,height=1  ,fg="black"  ,bg="white" ,font=('times', 17, ' bold ') )
-    lbl.place(x=0, y=55)
-
-    global txt
-    txt = tkinter.Entry(frame1,width=32 ,fg="black",bg="#e1f2f2",highlightcolor="#00aeff",highlightthickness=3,font=('times', 15, ' bold '))
-    txt.place(x=55, y=88,relwidth=0.75)
-
-    lbl2 = tkinter.Label(frame1, text="Enter Name",width=20  ,fg="black"  ,bg="white" ,font=('times', 17, ' bold '))
-    lbl2.place(x=0, y=140)
-    global txt2
-    txt2 = tkinter.Entry(frame1,width=32 ,fg="black",bg="#e1f2f2",highlightcolor="#00aeff",highlightthickness=3,font=('times', 15, ' bold ')  )
-    txt2.place(x=55, y=173,relwidth=0.75)
-
-    global message0
-    message0=tkinter.Label(frame1,text="Follow the steps...",bg="white" ,fg="black"  ,width=39 ,height=1,font=('times', 16, ' bold '))
-    message0.place(x=7,y=275)
-
-    global message1
-    message1 = tkinter.Label(frame1, text="1)Take Images  ===> 2)Save Profile" ,bg="white" ,fg="black"  ,width=39 ,height=1, activebackground = "yellow" ,font=('times', 15, ' bold '))
-    message1.place(x=7, y=300)
-
-    global message
-    message = tkinter.Label(frame1, text="" ,bg="white" ,fg="black"  ,width=39,height=1, activebackground = "yellow" ,font=('times', 16, ' bold '))
-    message.place(x=7, y=500)
+    
     #Attendance frame
     lbl3 = tkinter.Label(frame2, text="Attendance Table",width=20  ,fg="black"  ,bg="white"  ,height=1 ,font=('times', 17, ' bold '))
     lbl3.place(x=100, y=115)
 
-    #Display total registration----------
-    res=0
-    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
-    if exists:
-        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
-            reader1 = csv.reader(csvFile1)
-            for l in reader1:
-                res = res + 1
-        res = (res // 2) - 1
-        csvFile1.close()
-    else:
-        res = 0
-    message.configure(text='Total Registrations : '+str(res))
+    
 
     #BUTTONS----------------------------------------------
 
-    clearButton = tkinter.Button(frame1, text="Clear", command=clear, fg="white", bg="#13059c", width=11, activebackground = "white", font=('times', 12, ' bold '))
-    clearButton.place(x=55, y=230,relwidth=0.29)
-
-    takeImg = tkinter.Button(frame1, text="Take Images", command=TakeImages, fg="black", bg="#00aeff", width=34, height=1, activebackground = "white", font=('times', 16, ' bold '))
-    takeImg.place(x=30, y=350,relwidth=0.89)
-
-    trainImg = tkinter.Button(frame1, text="Save Profile", command=psw, fg="black", bg="#00aeff", width=34, height=1, activebackground = "white", font=('times', 16, ' bold '))
-    trainImg.place(x=30, y=430,relwidth=0.89)
+    
 
     trackImg = tkinter.Button(frame2, text="Processed Login", command=TrackImages, fg="black", bg="#00aeff", height=1, activebackground = "white" ,font=('times', 16, ' bold '))
     trackImg.place(x=30,y=60,relwidth=0.89)
@@ -513,37 +466,64 @@ def halaman2():
     message3.place(x=10, y=10,relwidth=1)
     
     #frame
-    frame = tkinter.Frame(window, bg="white")
-    frame.place(relx=0.32, rely=0.15, relwidth=0.39, relheight=0.80)
     
-    fr_head = tkinter.Label(frame, text="Register", fg="white",bg="black" ,font=('times', 17, ' bold ') )
-    fr_head.place(x=0,y=0,relwidth=1)
+    frame1 = tkinter.Frame(window, bg="white")
+    frame1.place(relx=0.11, rely=0.15, relwidth=0.39, relheight=0.80)
     
-    #Attandance table----------------------------
-    # style = ttk.Style()
-    # style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri', 11)) # Modify the font of the body
-    # style.configure("mystyle.Treeview.Heading",font=('times', 13,'bold')) # Modify the font of the headings
-    # style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})]) # Remove the borders
-    # global tbl 
-    # tbl= ttk.Treeview(frame,height =13,columns = ('name','date','time'),style="mystyle.Treeview")
-    # tbl.column('#0',width=82)
-    # tbl.column('name',width=130)
-    # tbl.column('date',width=133)
-    # tbl.column('time',width=133)
-    # tbl.grid(row=2,column=0,padx=(0,0),pady=(50,0),columnspan=4)
-    # tbl.heading('#0',text ='ID')
-    # tbl.heading('name',text ='NAME')
-    # tbl.heading('date',text ='DATE')
-    # tbl.heading('time',text ='TIME')
+    fr_head1 = tkinter.Label(frame1, text="Register", fg="white",bg="black" ,font=('times', 17, ' bold ') )
+    fr_head1.place(x=0,y=0,relwidth=1)
     
-    # #SCROLLBAR--------------------------------------------------
+    #registretion frame
+    lbl = tkinter.Label(frame1, text="Enter ID",width=20  ,height=1  ,fg="black"  ,bg="white" ,font=('times', 17, ' bold ') )
+    lbl.place(x=0, y=55)
 
-    # scroll=ttk.Scrollbar(frame,orient='vertical',command=tbl.yview)
-    # scroll.grid(row=2,column=4,padx=(0,100),pady=(150,0),sticky='ns')
-    # tbl.configure(yscrollcommand=scroll.set)
+    global txt
+    txt = tkinter.Entry(frame1,width=32 ,fg="black",bg="#e1f2f2",highlightcolor="#00aeff",highlightthickness=3,font=('times', 15, ' bold '))
+    txt.place(x=55, y=88,relwidth=0.75)
+
+    lbl2 = tkinter.Label(frame1, text="Enter Name",width=20  ,fg="black"  ,bg="white" ,font=('times', 17, ' bold '))
+    lbl2.place(x=0, y=140)
+    global txt2
+    txt2 = tkinter.Entry(frame1,width=32 ,fg="black",bg="#e1f2f2",highlightcolor="#00aeff",highlightthickness=3,font=('times', 15, ' bold ')  )
+    txt2.place(x=55, y=173,relwidth=0.75)
+
+    global message0
+    message0=tkinter.Label(frame1,text="Follow the steps...",bg="white" ,fg="black"  ,width=39 ,height=1,font=('times', 16, ' bold '))
+    message0.place(x=7,y=275)
+
+    global message1
+    message1 = tkinter.Label(frame1, text="1)Take Images  ===> 2)Save Profile" ,bg="white" ,fg="black"  ,width=39 ,height=1, activebackground = "yellow" ,font=('times', 15, ' bold '))
+    message1.place(x=7, y=300)
+
+    global message
+    message = tkinter.Label(frame1, text="" ,bg="white" ,fg="black"  ,width=39,height=1, activebackground = "yellow" ,font=('times', 16, ' bold '))
+    message.place(x=7, y=500)
     
-    quitWindow = tkinter.Button(frame, text="Quit", command=window.destroy, fg="white", bg="#13059c", width=35, height=1, activebackground = "white", font=('times', 16, ' bold '))
-    quitWindow.place(x=30, y=450,relwidth=0.89)
+    #Display total registration----------
+    res=0
+    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+    if exists:
+        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+            reader1 = csv.reader(csvFile1)
+            for l in reader1:
+                res = res + 1
+        res = (res // 2) - 1
+        csvFile1.close()
+    else:
+        res = 0
+    message.configure(text='Total Registrations : '+str(res))
+    
+    clearButton = tkinter.Button(frame1, text="Clear", command=clear, fg="white", bg="#13059c", width=11, activebackground = "white", font=('times', 12, ' bold '))
+    clearButton.place(x=55, y=230,relwidth=0.29)
+
+    takeImg = tkinter.Button(frame1, text="Take Images", command=TakeImages, fg="black", bg="#00aeff", width=34, height=1, activebackground = "white", font=('times', 16, ' bold '))
+    takeImg.place(x=30, y=350,relwidth=0.89)
+
+    trainImg = tkinter.Button(frame1, text="Save Profile", command=psw, fg="black", bg="#00aeff", width=34, height=1, activebackground = "white", font=('times', 16, ' bold '))
+    trainImg.place(x=30, y=430,relwidth=0.89)
+    
+    # quitWindow = tkinter.Button(frame1, text="Quit", command=window.destroy, fg="white", bg="#13059c", width=35, height=1, activebackground = "white", font=('times', 16, ' bold '))
+    # quitWindow.place(x=30, y=450,relwidth=0.89)
     
     #closing lines------------------------------------------------
     window.protocol("WM_DELETE_WINDOW", on_closing)
@@ -553,4 +533,4 @@ def halaman2():
 
 
 # run app
-halaman1()
+halaman2()
